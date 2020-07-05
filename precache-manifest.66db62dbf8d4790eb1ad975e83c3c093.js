f461d6e8f0c9e86d54a50fcb02ddda03cf78db24
דור חבסוב <dorhabasov@gmail.com>
2020-07-05 13:34:59 +0300

    Updates

---
+++ precache-manifest.66db62dbf8d4790eb1ad975e83c3c093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0607dfd73fc985fed42e9f982fc4445",
+    "url": "/DorProJobMap/index.html"
+  },
+  {
+    "revision": "c133fb550d05e5969206",
+    "url": "/DorProJobMap/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a1c6f308729613d909f3",
+    "url": "/DorProJobMap/static/js/2.0af463e5.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/DorProJobMap/static/js/2.0af463e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c133fb550d05e5969206",
+    "url": "/DorProJobMap/static/js/main.a081b7d9.chunk.js"
+  },
+  {
+    "revision": "eafc04a61fab5926d90e",
+    "url": "/DorProJobMap/static/js/runtime-main.27b4116b.js"
+  }
+]);
